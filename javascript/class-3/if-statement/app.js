1326291f8e0dcad6f0183a6031466ef1870765a5
Abdul-majid-ashrafi <dear_majid01@yahoo.com>
2020-03-06 13:00:58 +0500

    covered 10 to 17 chapter

---
+++ javascript/class-3/if-statement/app.js
@@ -0,0 +1,104 @@
+// if statements
+
+// where did was died Quid-e-Azam
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/** Logical operators **/
+
+        // AND - &&
+        // OR - ||
+        // NOT - !
+
+        //        true && true   = true
+        //        false && true  = false
+        //        false && false = false
+        //        true && false  = false
+
+        //true || true   = true
+        //false || true  = true
+        //false || false = false
+        //true || false  = true
+
+
+
+        // /** Testing multiple conditions in one statement **/
+
+        // var test = "JS";
+        // var score = 65;
+
+        // if (test === "CSS" && score > 60) {
+        //     alert("You have passed CSS test");
+        // }
+        // if (test === "JS" && score > 70) {
+        //     alert("You have passed JS test");
+        // }
+
+
+
+        // var age = 23;
+        // var residence = "UK";
+        // var IELTSScore = 8.2;
+
+        // if(age > 20 || residence == "US"){
+        //     alert("You can proceed");
+        // }
+
+        // if(age > 20 || (residence == "US" && IELTSScore > 8.0)){
+        //     alert("You can proceed");
+        // }
+
+
+
+
+        // var time = parseInt(prompt("Enter time in 24 hour format E.g. 1300 for 1pm", "1300"));
+        // if (time >= 0000 && time < 1200) {
+        //     message = "Good morning!";
+        // } else if (time >= 1200 && time < 1700) {
+        //     message = "Good afternoon!";
+        // } else if (time >= 1700 && time < 2100) {
+        //     message = "Good evening!";
+        // } else if (time >= 2100 && time <= 2359) {
+        //     message = "Good night!";
+        // }
+        // alert(message);
+
+/** Ternary Operator **/
+
+
+        // var isConfirmed = true;
+        // var status;
+        // if (isConfirmed == true) {
+        //     status = "Confirmed";
+        // }
+        // else {
+        //     status = "Pending";
+        // }
+        // alert(status)
+        // status = isConfirmed == true ? "Confirmed" : "Pending";
+        // alert(status)
